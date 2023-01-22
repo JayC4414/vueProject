2636a666ca6437010ce003d071eeda39b1fe0b60
Yizhao Chen <yizhao@jelp.delivery>
2023-01-21 16:37:39 -0800

    first commit

---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="greetings">
+      Todo list goes here.
+  </div>
+</template>
+
+<Script>
+export default {
+  name: 'todo-list',
+  data (){
+      return {
+          msg: 'welcome to your Vue.js App'
+      }
+  }
+}
+</Script>
+
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0;
+}
+
+a {
+  color: #42b983;
+}
+</style>
